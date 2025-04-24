428f10de7cf630e713eab04b0168c4355888bdcb
imzackadams <imzackadams@protonmail.com>
2025-04-24 18:41:45 -0400

    update readme

--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
 from setuptools import setup, find_packages
 
+# Load README.md as long_description for PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='api_diagnostic_tool',  # Your unique package name
-    version='1.0.1',
-    description='A CLI tool for API diagnostics',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/api_diagnostic_tool',
+    name='pingpoint',  # PyPI project name
+    version='1.0.1',  # Bump version from 1.0.0 for new release
+    description='A fast CLI tool for HTTP, DNS, and SSL diagnostics',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Zack Adams',
+    author_email='imZackAdams@protonmail.com',
+    url='https://github.com/ImZackAdams/pingpoint',
     packages=find_packages(),
     install_requires=[
         'click',
         'requests'
     ],
-    entry_points='''
-        [console_scripts]
-        api-diagnostic=api_diagnostic_tool.cli:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'pingpoint=pingpoint.cli:cli',
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Networking :: Monitoring",
     ],
     python_requires='>=3.6',
 )
